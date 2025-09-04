4d40635e9fe898723dcc5bf7d60726614f2c7f76
angelDavid24 <angeldavidnavatrujillo@gmail.com>
2025-09-04 13:11:19 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { Client } = require("pg");
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 async function connectWithRetry() {
   const client = new Client({
